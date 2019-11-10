132775b65ac8f19d63c539e87adf1e59847ffb37
Ilari Tommiska <ilari97@hotmail.com>
2019-11-10 15:52:29 +0200

    Koodi rikkoo nyt kaikkia checkstyle-sääntöjä

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,6 +5,18 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
+        for( int i=0; i<1; i++ ) {
+            for( int j=0; i<j; j++ ) {
+                System.out.println("virhe");
+            }
+        }
+        if (true) {
+            if (true) {
+                if (true) {
+                        System.out.println("virhe");
+                }
+            }
+        }
 
         Varasto mehua = new Varasto(100.0);
         Varasto olutta = new Varasto(100.0, 20.2);
